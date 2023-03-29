85a2ddda80e733b27ff421c7d9cec5733af6b0da
Gabriel Torres <gabrieltorresgts@gmail.com>
2023-03-28 22:35:09 -0300

    - Added Delta Time Factor

--- Game.cpp
+++ Game.cpp
@@ -90,7 +90,7 @@ glm::vec2 playerVelocity;
 void Game::Setup()
 {
 	playerPosition = glm::vec2(10.0, 20.0);
-	playerVelocity = glm::vec2(1.0, 0.0);
+	playerVelocity = glm::vec2(100.0, 0.0);
 }
 
 void Game::Update()
@@ -101,12 +101,15 @@ void Game::Update()
 	{
 		SDL_Delay(timeToWait);
 	}
+
+	//The difference in ticks since the last frame, converted to seconds
+	double deltaTime = (SDL_GetTicks() - millisecsPreviousFrame) / 1000.0f;
 	
 	// Store the current frame time
 	millisecsPreviousFrame = SDL_GetTicks();
 
-	playerPosition.x += playerVelocity.x;
-	playerPosition.y += playerVelocity.y;
+	playerPosition.x += playerVelocity.x * deltaTime;
+	playerPosition.y += playerVelocity.y * deltaTime;
 }
 
 void Game::Render()
